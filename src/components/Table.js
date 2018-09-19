b4aa7b48ef1fbcb204b390b5dc2db0c8a0d35f80
Adrian Ross <adriantoddross@gmail.com>
2018-09-18 23:04:16 -0400

    create list map and display it in <tbody>

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import leaderboard from '../leaderboard.json';
 
-export default function table(props) {
+export default function table() {
 
-  // const players = leaderboard.map((player) => {
-    
-  // });
+  const players = leaderboard.data.map((player) => 
+    <tr key={player.username}>
+      <td></td>
+      <td>{player.username}</td>
+      <td>{player.missionsPlayed}</td>
+      <td>{player.bounty}</td>
+    </tr>
+  );
 
   return (
     <table>
-      <tr>
-        <th>Rank</th>
-        <th>Username</th>
-        <th>Missions Played</th>
-        <th>Bounty</th>
-      </tr>
-      {/* From here on out, population the table with the JSON data!  */}
+      <thead>
+        <tr>
+          <th>Rank</th>
+          <th>Username</th>
+          <th>Missions Played</th>
+          <th>Bounty</th>
+        </tr>
+      </thead>
+      <tbody>
+        {players}
+      </tbody>
     </table>
   );
 }
