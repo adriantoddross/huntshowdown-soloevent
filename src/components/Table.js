2760732159c099a239434f9b61d90e5b7a879a5e
Adrian Ross <adriantoddross@gmail.com>
2018-09-18 22:25:38 -0400

    draft table component

---
+++ src/components/Table.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import leaderboard from './leaderboard.JSON';
+
+export default function table(props) {
+
+// const players =
+
+  return (
+    <table>
+      <tr>
+        <th>Rank</th>
+        <th>Username</th>
+        <th>Missions Played</th>
+        <th>Bounty</th>
+      </tr>
+      {/* From here on out, population the table with the JSON data!  */}
+    </table>
+  );
+}
