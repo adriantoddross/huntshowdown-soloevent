5fcb4394cea9a4faa762b794cbea77f3b16452e1
Adrian Ross <adriantoddross@gmail.com>
2018-09-19 20:50:34 -0400

    initilalize leaderboard data in the state

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import leaderboard from '../leaderboard.json';
 
 class leaderboardTable extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {data: [leaderboard.data]};
+  }
 
   render() {
 
