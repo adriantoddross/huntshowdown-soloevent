20a0a130638b7fdf4b8264857f3bd6b0b1c29439
Adrian Ross <adriantoddross@gmail.com>
2018-09-19 20:43:52 -0400

    convert function into class

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import leaderboard from '../leaderboard.json';
 
-export default function table() {
+class leaderboardTable extends Component {
 
-  const players = leaderboard.data.map((player, index) => 
-    <tr key={player.username}>
-      <td>{index + 1}</td>
-      <td>{player.username}</td>
-      <td>{player.missionsPlayed}</td>
-      <td>{player.bounty}</td>
-    </tr>
-  );
+  render() {
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Username</th>
-          <th>Missions Played</th>
-          <th>Bounty</th>
-        </tr>
-      </thead>
-      <tbody>
-        {players}
-      </tbody>
-    </table>
-  );
-}
+    const players = leaderboard.data.map((player, index) => 
+      <tr key={player.username}>
+        <td>{index + 1}</td>
+        <td>{player.username}</td>
+        <td>{player.missionsPlayed}</td>
+        <td>{player.bounty}</td>
+      </tr>
+    );
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Username</th>
+            <th>Missions Played</th>
+            <th>Bounty</th>
+          </tr>
+        </thead>
+        <tbody>
+          {players}
+        </tbody>
+      </table>
+    );
+  }
+  }
+  
+export default leaderboardTable;
