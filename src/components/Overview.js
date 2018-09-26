6237e6fe716663501777b920aa3eb30004caaa8a
Adrian Ross <adriantoddross@gmail.com>
2018-09-26 00:14:21 -0400

    add comments

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -8,14 +8,14 @@ export default function preview() {
   // Let's use a helper function to get the sum of whatever key/value pair we're looking for using .reduce.
   const sum = (key, array) => array.reduce(function (acc, obj) { return acc + parseInt(obj[key], 10); }, 0);
 
-  const sumOfBounties = sum('bounty', leaderboardData);
-  const averageBounty = sumOfBounties / leaderboardData.length;
+  const sumOfBounties = sum('bounty', leaderboardData); // we use our helper function for the sum
+  const averageBounty = sumOfBounties / leaderboardData.length; // the average is the sum divided by the count
 
   const sumOfMissionsPlayed = sum('missionsPlayed', leaderboardData);
   const averageMissionsPlayed = sumOfMissionsPlayed / leaderboardData.length;
 
 
-  const average = sum / leaderboardData.length; // average is the sum divided by the count
+  const average = sum / leaderboardData.length; 
 
   return (
     <div>
