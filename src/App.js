e5028b5b2e2fa4d1a8a40af85c6bdde46caff777
Adrian Ross <adriantoddross@gmail.com>
2018-09-26 00:14:59 -0400

    import Overview component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Overview from './components/Overview';
 import Table from './components/Table';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Overview/>
         <Table/>
       </div>
     );
