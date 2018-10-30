31f062051b56b6519ba1a5617bd6da19f28b529d
Adrian Ross <adriantoddross@gmail.com>
2018-10-30 13:35:10 -0400

    Initialize state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Leaderboard from './leaderboard.json'; // import leaderboard JSON data
 import Overview from './components/Overview';
 import Table from './components/Table';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props); 
+    this.state = {data: [...Leaderboard.data]}; // use spread operator to copy the leaderboard array into the state!
+  }
+
   render() {
     return (
       <div>
