80bb3544e9da3c9d2032abdaa2dc458a534f36be
Adrian Ross <adriantoddross@gmail.com>
2018-10-31 14:22:26 -0400

    Stub Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navbar(props){
+  return(
+    <nav>
+      <img src="" alt=""/>
+      <ul>
+        <li>About</li>
+        <li>Learn About Hunt: Showdown</li>
+        {/* <li></li> */}
+      </ul>
+    </nav>
+  );
+}
