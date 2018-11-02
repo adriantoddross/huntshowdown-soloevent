16248c73559b0a444c0f0598af0eb72a23ed77b7
Adrian Ross <adriantoddross@gmail.com>
2018-11-02 11:56:16 -0400

    Revise class names

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,13 @@ import './Navbar.css';
 
 export default function Navbar(){
   return(
-    <nav className='nav'>
-      <img src="" alt=""/>
-      <ul className='nav-list'>
-        <li className='nav-item'>
+    <nav className='navbar'>
+      <img className='navbar-img' src="" alt=""/>
+      <ul className='navbar-list'>
+        <li className='navbar-item'>
           <a href="https://github.com/adriantoddross/huntshowdown-soloevent" target="_blank" rel="noopener noreferrer">About</a>
         </li>
-        <li className='nav-item'>
+        <li className='navbar-item'>
           <a href="https://www.huntshowdown.com/" target="_blank" rel="noopener noreferrer">What is Hunt: Showdown?</a>
         </li>
       </ul>
