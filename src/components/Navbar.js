b1ce89a28d42ba0ff0e81081f80e5a7f6730deb8
Adrian Ross <adriantoddross@gmail.com>
2018-11-02 12:43:47 -0400

    Add menu label for a11y

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,13 @@ import './Navbar.css';
 
 export default function Navbar(){
   return(
-    <nav className='navbar'>
-      <img className='navbar-img' src="https://res.cloudinary.com/adriantoddross/image/upload/v1541175017/Adrian-Letter-A.png" alt="null"/>
+    <nav className='navbar' aria-labelledby="navigationmenu">
       <ul className='navbar-list'>
+        <li className='navbar-item'>
+          <a href="https://www.adriantoddross.com/" target="_blank" rel="noopener noreferrer">
+            <img className='navbar-img' src="https://res.cloudinary.com/adriantoddross/image/upload/v1541175017/Adrian-Letter-A.png" alt="null"/>
+          </a>
+        </li>
         <li className='navbar-item'>
           <a href="https://github.com/adriantoddross/huntshowdown-soloevent" target="_blank" rel="noopener noreferrer">About</a>
         </li>
