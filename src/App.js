536f69ba108d2f96b6e7e2d040021cbe97742233
Adrian Ross <adriantoddross@gmail.com>
2018-11-05 11:08:48 -0500

    Render Summary component & transfer out state

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Leaderboard from './leaderboard.json'; // import leaderboard JSON data
 import Navbar from './components/Navbar';
-import Overview from './components/Overview';
-import Table from './components/Table';
+import Summary from './components/Summary';
 import Footer from './components/Footer';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props); 
-    this.state = {data: [...Leaderboard.data]}; // use spread operator to copy the leaderboard array into the state!
-
-    this.sortByString = this.sortByString.bind(this);
-    this.sortByNumber = this.sortByNumber.bind(this);
-  }
-
-  sortByNumber(num) {
-    let players = Object.assign({}, this.state);
-    players.data.sort(function(a,b) {return b[num] - a[num];});
-    this.setState({players});
-  }
-  
-  sortByString(key) {
-    let players = Object.assign({}, this.state);
-    players.data.sort(function(a,b) {return a[key].localeCompare(b[key]);});
-    this.setState({players});
-  }
 
   render() {
     return (
       <div>
         <Navbar/>
-        <Overview
-          stats={this.state.data}
-        />
-        <Table 
-          stats={this.state.data}
-          sortByUsername={this.sortByString}
-          sortByMissionsPlayed={this.sortByNumber}
-          sortByBounty={this.sortByNumber}
-        />
+        <Summary/>
         <Footer/>
       </div>
     );
